841bf849844eb2ae83c0e1727ed28b5f2e7fadac
Jonathan Somer <somer@mail.tau.ac.il>
2017-05-11 17:02:26 +0300

    c

--- RayTracingRenderer.java
+++ RayTracingRenderer.java
@@ -4,7 +4,10 @@ public class RayTracingRenderer implements IRenderer {
 
 	@Override
 	public boolean renderScene(Scene scene, String pathToResultImage, int resultImageWidth, int resultImageHeight) {
-		// TODO Auto-generated method stub
+
+
+		
+		
 		return false;
 	}
 
