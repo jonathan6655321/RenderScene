e84dc01be337995ddd3f1102d5b2783b65cfa0c6
Jonathan Somer <somer@mail.tau.ac.il>
2017-05-14 11:56:01 +0300

    vector 3d work, and camera

--- Camera.java
+++ Camera.java
@@ -1,19 +1,56 @@
 package RenderScene;
 
 public class Camera {
-	private float[] position = new float[3]; 
-	private float[] lookAtVector = new float[3]; // will also be the view plane normal
-	private float[] upVector = new float[3]; // direction cameras top points at
-	private float screenWidthRelativeToScene;
-	private float screenDistance;
+	private Vector3D position;; 
+	private Vector3D lookAtDirection; // will also be the view plane normal
+	private Vector3D upDirection; // direction cameras top points at
+	private double screenWidthRelativeToScene;
+	private double screenDistance;
+	
+	//Screen params - initialized through setScreenParams. 
+	private Vector3D screenCenterPosition;
+	private double pixelWidth; // also height
+	private Vector3D topLeftPixelPosition;
+	private Vector3D onePixelRightDiff;
+	private Vector3D onePixelDownVector;
 	
 	
+	// TODO is upDirection perpendicular to LookAtDirection
+	public void setScreenParams(int imageHeight, int imageWidth)
+	{
+		initScreenCenterPosition();
+		pixelWidth = screenWidthRelativeToScene / imageWidth;
+		initOnePixelDownDiff();
+		
+	}
 	
+	// iamge height, width is number of pixels 
 	public Ray getRayWhichLeavesFromPixel(int col, int row, int imageHeight, int imageWidth)
 	{
+		Ray ray = new Ray();
+		
+		
+		
+		
+		
 		return null;
 	}
 	
 	
+	// Related to screen params init
+	private void initScreenCenterPosition()
+	{
+		lookAtDirection.normalize();
+		screenCenterPosition = Vector3D.add(position, lookAtDirection.getVectorMultipliedByConstant(screenDistance));
+	}
+	
+	private void initOnePixelDownDiff()
+	{
+		
+		upDirection.normalize();
+		
+		onePixelDownVector =  
+				upDirection.getVectorMultipliedByConstant(pixelWidth).getReversedVector();
+	}
 	
 }
--- Vector3D.java
+++ Vector3D.java
@@ -1,5 +1,70 @@
 package RenderScene;
 
 public class Vector3D {
-	public float[] vector = new float[3];
+	public double[] vector = new double[3];
+	
+	public Vector3D(double x, double y, double z)
+	{
+		this.vector[0] = x;
+		this.vector[1] = y;
+		this.vector[2] = z;
+	}
+	
+	public Vector3D(Vector3D startPoint, Vector3D endPoint)
+	{
+		this.vector = subtract(endPoint, startPoint).vector;
+	}
+	
+	
+	public static Vector3D add(Vector3D vector1, Vector3D vector2)
+	{
+		Vector3D sumVector = new Vector3D
+		(vector1.vector[0] + vector2.vector[0], 
+		 vector1.vector[1] + vector2.vector[1],
+		 vector1.vector[2] + vector2.vector[2]); 
+		
+		return sumVector;
+	}
+	
+	public static Vector3D subtract(Vector3D vector1, Vector3D vector2)
+	{
+		Vector3D diffVector = new Vector3D
+		(vector1.vector[0] - vector2.vector[0], 
+		 vector1.vector[1] - vector2.vector[1],
+		 vector1.vector[2] - vector2.vector[2]); 
+		
+		return diffVector;
+	}
+	
+	public double getMagnitude()
+	{
+		return Math.sqrt(Math.pow((this.vector[0]),2) + 
+						 Math.pow((this.vector[1]),2) + 
+						 Math.pow((this.vector[2]),2));
+	}
+	
+	public void multiplyByConstant(double c)
+	{
+		this.vector[0] = c*this.vector[0]; 
+		this.vector[1] = c*this.vector[1];
+		this.vector[2] = c*this.vector[2];
+	}
+	
+	public Vector3D getVectorMultipliedByConstant(double c)
+	{
+		Vector3D v  = new Vector3D(c*this.vector[0], c*this.vector[1], c*this.vector[2]);
+		return v;
+	}
+	
+	public Vector3D getReversedVector()
+	{
+		return getVectorMultipliedByConstant(-1);
+	}
+	
+	// changes the vector!!
+	public void normalize()
+	{
+		this.multiplyByConstant(this.getMagnitude());
+	}
+	
 }
