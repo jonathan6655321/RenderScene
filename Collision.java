bb2952f7c351d4eff5a176a32549b4dcd6efd733
GameXtra <Kessido@Gmail.com>
2017-05-14 15:00:38 +0300

    collition

--- Collision.java
+++ Collision.java
@@ -4,12 +4,18 @@ public class Collision {
 	
 	private RenderableObject collisionObject;
 	private Vector3D collisionPoint;
-	private Vector3D normalToCollisionPoint;
+	private Vector3D normalToCollisionPoint = null;
 	
-	public Collision(RenderableObject collisionObject, Vector3D collisionPoint, Vector3D normalToCollisionPoint)
+	public Collision(RenderableObject collisionObject, Vector3D collisionPoint)
 	{
 		this.collisionObject = collisionObject;
 		this.collisionPoint = collisionPoint;
-		this.normalToCollisionPoint = normalToCollisionPoint;
+	}
+	
+	public Vector3D getNormalToCollisionPoint(){
+		if(normalToCollisionPoint == null){
+			normalToCollisionPoint= collisionObject.getNormalAtPoint(collisionPoint);
+		}
+		return normalToCollisionPoint;
 	}
 }
