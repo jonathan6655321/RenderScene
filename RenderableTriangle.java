d459a473c8458fcdf0981a627e546cc530b555c5
GameXtra <Kessido@Gmail.com>
2017-05-14 22:44:58 +0300

    triangle tries

--- RenderableTriangle.java
+++ RenderableTriangle.java
@@ -1,5 +1,7 @@
 package RenderScene;
 
+import javax.management.RuntimeErrorException;
+
 public class RenderableTriangle extends RenderablePlane {
 	Vector3D triangleVertex;
 	Vector3D[] triangleVectors = new Vector3D[3];
@@ -14,8 +16,19 @@ public class RenderableTriangle extends RenderablePlane {
 			this.triangleVectors[i].normalize();
 		}
 		Vector3D avrageOfVertex1Vertex2 = Vector3D.add(vertices[1], vertices[2]).getVectorMultipliedByConstant(0.5);
-		this.triangleVectors[2] =Vector3D.subtract(avrageOfVertex1Vertex2, triangleVertex);
+		Vector3D thirdVector = Vector3D.subtract(vertices[1], vertices[2]);
+
+		Vector3D medianVector = Vector3D.subtract(avrageOfVertex1Vertex2, triangleVertex);
+		Vector3D perpendicularVector = Vector3D.crossProduct(thirdVector, planeNormal);
+		perpendicularVector.normalize();
+
 		
+		double pendicularVectorComponentInMedian = Vector3D.dotProduct(perpendicularVector, medianVector);
+		perpendicularVector = perpendicularVector.getVectorMultipliedByConstant(pendicularVectorComponentInMedian);
+		
+		double senityCheck = Vector3D.dotProduct(perpendicularVector, thirdVector);
+
+		this.triangleVectors[2] = perpendicularVector;
 		this.vectorsMagnitude[2] = this.triangleVectors[2].getMagnitude();
 		this.triangleVectors[2].normalize();
 	}
