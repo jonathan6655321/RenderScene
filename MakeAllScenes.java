6b481b2745b2263fbd8aaf17159b473b30c6beba
GameXtra <Kessido@Gmail.com>
2017-06-04 13:58:24 +0300

    a

--- MakeAllScenes.java
+++ MakeAllScenes.java
@@ -1,15 +1,15 @@
 package RenderScene;
 
-import RenderScene.Bonus.convertOBJToParserableFormat;
-
 public class MakeAllScenes {
-	public final static String defultFolder = convertOBJToParserableFormat.IDO_FOLDER;
-	public final static String sceneFolder = "Scenes\\";
-	public final static String[] filesToRender = { "Transparency", "Pool", "Room1", "Room10", "Spheres", "Triangle",
+	private static String IDO_FOLDER = "F:\\Tau\\Courses\\Computer Science\\2016-2017\\Semester 2\\Graphic\\RenderScene\\git\\RenderScene\\";
+	//private static String JOHNATHAN_FOLDER = "C:\\Development\\Graphics\\RenderScene\\";
+	private final static String defultFolder = IDO_FOLDER;
+	private final static String sceneFolder = "Scenes\\";
+	private final static String[] filesToRender = { "Transparency", "Pool", "Room1", "Room10", "Spheres", "Triangle",
 			"Triangle2" };
-	public final static String fileType = ".txt";
-	public final static String destFolder = "MyScenes\\";
-	public final static String destFileType = ".png";
+	private final static String fileType = ".txt";
+	private final static String destFolder = "MyScenes\\";
+	private final static String destFileType = ".png";
 
 	public static void main(String[] args) {
 		for (String fileToRender : filesToRender) {
